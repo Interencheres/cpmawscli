86176b46805abba240fd77b1bfbdc15d86ccb422
François Pietka <francois@pietka.fr>
2016-09-27 16:59:10 +0200

    fix(rds): filter wasn't working anymore for RDS

--- CpmAWS/classes/RdsCollection.py
+++ CpmAWS/classes/RdsCollection.py
@@ -1,4 +1,5 @@
 import logging
+from fnmatch import fnmatch
 
 from CpmAWS.core.Collection import Collection
 from CpmAWS.classes.RdsInstance import RdsInstance
@@ -26,3 +27,21 @@ class RdsCollection(Collection):
         for name in self.instances:
             logging.debug(name)
         return self.instances
+
+    def instanceBelongsToFilter(self, instance):
+        """As filters are not available for describe_db_instances, we need to
+        implement it here
+        @see https://docs.aws.amazon.com/cli/latest/reference/rds/describe-db-instances.html
+        """
+        for tag in self.parameters.tags:
+            key = tag["Name"][4:]
+            values = tag["Values"]
+            if key == "Name":
+                for name in values:
+                    if not fnmatch(instance.name, name):
+                        return False
+            else:
+                if instance.tags.get(key) not in values:
+                    return False
+
+        return super(RdsCollection, self).instanceBelongsToFilter(instance)
